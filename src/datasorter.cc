a65cc0baaba9e04776bb5bc296df86b4426c6a36
sstorey <Sarah.Storey@cern.ch>
2023-02-28 11:40:59 +0100

    Using v3.0.0. for command line parser submodule

--- src/datasorter.cc
+++ src/datasorter.cc
@@ -7,8 +7,29 @@
 #include <iomanip>
 #include <numeric>
 #include <iostream>
+#include <getopt.h>
 
 int main(int argc, char* argv[])
 {
+    // std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
+
+    // option cCLOptions[] = {
+    //     {"number_of_lines_to_parse", optional_argument, NULL, 'n'}};
+    
     std::cout << "Datasorter bare-bones executable\n";
+    int  cOption=1;
+    while ((cOption = getopt (argc, argv, "abc:")) != -1)
+    {
+        // const int cCLOption = getopt_long(argc, argv, "nE::", cCLOptions, 0);
+        // if (cCLOption == -1) {
+        //     break;
+        // }
+
+        switch (cOption) {
+            case 'n':
+               auto cValue = optarg;
+               std::cout << "Will only sort through first " << cValue << " lines in the file\n";
+               break;
+        }
+    }
 }
